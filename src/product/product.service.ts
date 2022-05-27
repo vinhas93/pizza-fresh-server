573c4d94db458b0b57593bed570ef32d5c500287
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-05-27 16:55:43 -0300

    feat(product): implement controller

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(dto: CreateProductDto): Promise<Product> {
+    const data: Product = {...dto }
+    return this.prisma.
   }
 
   findAll() {
     return `This action returns all product`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} product`;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  update(id: string, dto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
+  delete(id: string) {
     return `This action removes a #${id} product`;
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('/n');
+    const lastErrorLine = errorLines[errorLines.length - 1].trim();
+    throw new UnprocessableEntityException(
+      lastErrorLine || 'Algum erro ocorreu ao executar a operação.',
+    );
   }
 }
