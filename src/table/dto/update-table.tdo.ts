9d43a354276b67fa02a224ce3503748ab54335f9
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-05-27 18:58:08 -0300

    fix(table): change 'PartialType'  to swagger on 'UpdateTableDto'

--- src/table/dto/update-table.tdo.ts
+++ src/table/dto/update-table.tdo.ts
@@ -1,11 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { ApiProperty } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreateTableDto } from './create-table.dto';
 
-export class UpdateTableDto extends PartialType(CreateTableDto) {
-  @ApiProperty({
-    description: 'O novo número da mesa.',
-    example: 100,
-  })
-  number: number;
-}
+export class UpdateTableDto extends PartialType(CreateTableDto) {}
