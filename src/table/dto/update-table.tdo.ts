2e5b0a73e781cdc8174ae10876fcc6cbf1c93a49
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-05-27 09:50:56 -0300

    doc: properties of 'createTableDto'

--- src/table/dto/update-table.tdo.ts
+++ src/table/dto/update-table.tdo.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateTableDto } from './create-table.dto';
 
-export class UpdateTableDto extends PartialType(CreateTableDto) {}
+export class UpdateTableDto extends PartialType(CreateTableDto) {
+  @ApiProperty({
+    description: 'O novo número da mesa.',
+    example: 100,
+  })
+  number: number;
+}
