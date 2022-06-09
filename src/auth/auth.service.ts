df57c31d0cf6c146be01d4a70cd2f0407b8ade9e
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-06-09 20:33:18 -0300

    feat(auth): generate resource

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { LoginResponseDto } from './dto/login-response.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async login(loginDto: LoginDto): Promise<LoginResponseDto> {
+    return {
+      token: 'teste',
+      user: undefined,
+    };
+  }
+}
