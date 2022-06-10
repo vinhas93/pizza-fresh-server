b56f18b6d5386b666163a359d5df416b241109e9
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-06-10 19:45:14 -0300

    chore: add cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
@@ -14,17 +16,17 @@ async function bootstrap() {
     .setVersion('1.0.0')
     .addTag('status')
     .addTag('auth')
-    .addTag('user')
-    .addTag('product')
     .addTag('table')
+    .addTag('product')
+    .addTag('user')
     .addTag('order')
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  console.log('http://www.localhost:3333/api');
 
   await app.listen(3333);
 }
+
 bootstrap();
