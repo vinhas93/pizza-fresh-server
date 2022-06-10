21e15551ef61b7f096562fb60082004b70c9b3ee
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-06-10 20:38:58 -0300

    chore: https config

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getAppStatus(): string {
-    return 'Server is running! 🚀\n\n Please check http://pizza-fresh-server-production-2893.up.railway.app/api for Swagger docs 😉';
+  getAppStatus(baseUrl: string) {
+    return {
+      status: 'Server is running! 🚀',
+      docs: baseUrl + '/api',
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
 
+  // Para conseguir pegar o protocolo https
+  app.set('trust proxy', 1);
+
+  // Validation
   app.useGlobalPipes(new ValidationPipe());
 
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle('PizzaFresh')
     .setDescription('Aplicação para gestão das mesas de uma pizzaria')
