9ef6234af6bb9723bc43570e6a4a49d090102b59
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-06-10 20:24:53 -0300

    chore: update status url

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getAppStatus(): string {
-    return 'Server is running! 🚀\n\n Please check http://localhost:3333/api for Swagger docs 😉';
+    return 'Server is running! 🚀\n\n Please check http://pizza-fresh-server-production-2893.up.railway.app/api for Swagger docs 😉';
   }
 }
